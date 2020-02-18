ca138f676b78b0d8950d010dcbe7b73f9a1c1c11
Sean Bailey <sean@spb.li>
2020-02-18 15:15:55 -0500

    display

--- assets/js/words.js
+++ assets/js/words.js
@@ -3,9 +3,32 @@ function getCurrent() {
 }
 
 function getNext() {
-    
+    return getCurrent().nextElementSibling;
 }
 
 function get(num) {
-    
+    id = 'w' + num;
+    return document.getElementById(id);
 }
+
+function animateCSS(element, animationName, callback) {
+    var node;
+
+    if (typeof(element) == "string") {
+      node = document.querySelector(element)
+    } else {
+      node = element;
+    }
+
+    node.classList.add('animated', animationName, 'faster')
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName, 'faster')
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+
