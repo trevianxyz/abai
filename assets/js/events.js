5bc83f09578c36bd7e68091097be6d4cc6114efe
Sean Bailey <sean@spb.li>
2020-02-21 23:02:10 -0500

    event rendering

--- assets/js/events.js
+++ assets/js/events.js
@@ -25,3 +25,105 @@ data.contents.forEach(function(e) {
         dates.push(d);
     }
 });
+
+function getEvent(title) {
+
+    var temp = null;
+
+    data.contents.forEach(function(e) {
+        if (e.title === title) {
+            temp = e;
+        }
+    })
+
+    return temp;
+}
+
+function renderEvent(event) {
+    console.log('rendering event ' + event)
+    if (typeof event === 'string') {
+        console.log('issa string')
+        event = getEvent(event);
+        console.log(event)
+    }
+
+    if (event !== null) {
+
+        if (event.src) {
+            document.getElementById('ed-img').src = event.src;
+        }
+
+        if (event.title) {
+            document.getElementById('ed-title').innerHTML = event.title;
+        } else {
+            document.getElementById('ed-title').innerHTML = "";
+        }
+
+        if (event.subtitle) {
+            document.getElementById('ed-subtitle').innerHTML = event.subtitle;
+        } else {
+            document.getElementById('ed-subtitle').innerHTML = "";
+        }
+
+        if (event['event-date']) {
+            document.getElementById('ed-date').innerHTML = event['event-date'];
+        } else {
+            document.getElementById('ed-date').innerHTML = "";
+        }
+
+        if (event['event-time']) {
+            document.getElementById('ed-time').innerHTML = event['event-time'];
+        } else {
+            document.getElementById('ed-time').innerHTML = "";
+        }
+
+        if (event['event-location']) {
+            document.getElementById('ed-location').innerHTML = event['event-location'];
+        } else {
+            document.getElementById('ed-location').innerHTML = "";
+        }
+
+        if (event['event-address']) {
+            document.getElementById('ed-address').innerHTML = event['event-address'];
+        } else {
+            document.getElementById('ed-address').innerHTML = "";
+        }
+
+        if (event.body) {
+            document.getElementById('ed-body').innerHTML = event.body;
+        } else {
+            document.getElementById('ed-body').innerHTML = "";
+        }
+
+        return 0;
+    }
+
+    return -1;
+}
+
+function closeEventListings() {
+    listings = document.querySelectorAll('.event-listing');
+    listings.forEach((l) => l.classList.add('d-none'));
+}
+
+function openEventListings() {
+    listings = document.querySelectorAll('.event-listing');
+    listings.forEach((l) => l.classList.remove('d-none'));
+}
+
+domready(function() {
+    
+    viewBtns = document.querySelectorAll('button.btn-view');
+
+    viewBtns.forEach(function(b) {
+        b.addEventListener('click', function(b) {
+            result = renderEvent(b.target.dataset.eventTitle);
+            if (result === 0) {
+                closeEventListings();
+            } else {
+                alert("Error: this event is currently unavailable.");
+            }
+        })
+    })
+
+})
