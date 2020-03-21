46f9d5504d7c6cbd4f9c3555bf3dd365d7b756eb
Sean Bailey <sean@spb.li>
2020-03-20 21:53:53 -0400

    canvas rednering

--- assets/js/discover.js
+++ assets/js/discover.js
@@ -1,2 +1,17 @@
 console.log("Hello from discover.js");
 
+function cb() {
+    var c = document.getElementById('disc-img');
+    var ctx = c.getContext('2d');
+    var img = document.getElementById('img1');
+    ctx.drawImage(img, 10, 10);
+}
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+    cb();
+} else {
+    document.addEventListener("DOMContentLoaded", cb);
+}
