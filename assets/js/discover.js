d4b5e65b6c45c65c8c7066ceb1b1fe691ac1331f
Sean Bailey <sean@spb.li>
2020-03-29 17:57:01 -0400

    refactor (add Canvas class, background image rendering)

--- assets/js/discover.js
+++ assets/js/discover.js
@@ -1,16 +1,45 @@
 console.log("Hello from discover.js");
 
+class Canvas {
+
+    constructor(elem) {
+        this._elem = typeof elem == 'string' ? document.getElementById(elem) : elem;
+        this._ctx = this._elem.getContext('2d');
+    }
+
+    get bg() {
+        return this._bg;
+    }
+
+    set bg(img) {
+        this._bg = typeof img == 'string' ? document.getElementById(img) : img;
+        this.redraw();
+    }
+
+    redraw() {
+        this._ctx.drawImage(this._bg, 0, 0);
+    }
+
+}
+
+
+
+
+
 function cb() {
-    var c = document.getElementById('disc-img');
-    //setDPI(c, 192);
-    var ctx = c.getContext('2d');
-    var img = document.getElementById('img1');
-    //ctx.drawImage(img, 10, 10);
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, c.width, c.height);
-    ctx.font = "18px Arial";
-    ctx.fillStyle = "black";
-    ctx.fillText("You are a little brick in the wall of the world. Find your place in it!", 20, 50);
+    var c = new Canvas('disc-img');
+    c.bg = 'img1';
+
+    // var c = document.getElementById('disc-img');
+    // setDPI(c, 192);
+    // var ctx = c.getContext('2d');
+    // var img = document.getElementById('img1');
+    // ctx.drawImage(img, 10, 10);
+    // ctx.fillStyle = "blue";
+    // ctx.fillRect(0, 0, c.width, c.height);
+    // ctx.font = "18px Arial";
+    // ctx.fillStyle = "black";
+    // ctx.fillText("You are a little brick in the wall of the world. Find your place in it!", 20, 50);
 }
 
 function setDPI(canvas, dpi) {
@@ -34,3 +63,4 @@ if (
 } else {
     document.addEventListener("DOMContentLoaded", cb);
 }
+
